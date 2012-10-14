fca076c47eaa37e6e19eb407de475c080945d690
Brett Gibson <bdg@brettdgibson.com>
2012-10-14 13:59:01 -0700

    hopefully this makes things work both when CFBundleShortVersionString is and is not set

--- lib/airbrake_symbolicate.rb
+++ lib/airbrake_symbolicate.rb
@@ -2,54 +2,48 @@ require 'active_resource'
 module AirbrakeSymbolicate
   class DsymFinder
     @@dsyms = nil
-    
+
     class << self
       def dsym_for_error(error)
         find_dsyms  unless @@dsyms
-        
-        @@dsyms[error.environment.application_version]
+
+        @@dsyms[error.app_version]
       end
-      
-      private 
-      
+
+      private
+
       # use spotlight to find all the xcode archives
       # then use the Info.plist inside those archives to try and look up a git commit hash
       def find_dsyms
         @@dsyms = {}
-        
-        files = `mdfind 'kMDItemKind = "Xcode Archive"'`.split("\n")
+
+        files = `mdfind -name ".xcarchive"`.split("\n")
         files.each do |f|
-          # puts `ls '#{f.chomp}'`
           info = `find '#{f}/Products' -name Info.plist`.chomp
 
-          # this is the version reported as application-version by Airbrake
-          version = plist_val(info, 'CFBundleVersion')
+          short_version = plist_val(info, 'CFBundleShortVersionString')
+          long_version = plist_val(info, 'CFBundleVersion')
+          version = if short_version
+            "#{short_version} (#{long_version})"
+          else
+            long_version
+          end
+
+          puts version
+
           if bin_file = Dir[File.join(f, '/dSYMs/*.dSYM/**/DWARF/*')].first
             @@dsyms[version] = bin_file
           end
-          
-          # I don't see the git-commit value in the values Airbrake
-          # returns for my errors. Maybe these are only added with github
-          # integration?
-          #if commit = plist_val(info, 'GCGitCommitHash')
-              #@@dsyms[commit] = bin_file
-            #end
-          #else
-          # The responses I get from Airbrake just have the CFBundleVersion value,
-          # not sure if there are some conditions where both versions below are returned.
-            #short_version = plist_val(info, 'CFBundleShortVersionString')
-            #long_version = plist_val(info, 'CFBundleVersion')
-            #@@dsyms["#{CFBundleShortVersionString} (#{CFBundleVersion})"] = bin_file
-          #end
+
         end
       end
-      
+
       def plist_val(plist, key)
         `/usr/libexec/PlistBuddy -c 'Print :#{key}' '#{plist}' 2>/dev/null`.chomp
       end
     end
   end
-  
+
   class Symbolicator
     class << self
       def symbolicated_backtrace(error)
@@ -57,7 +51,7 @@ module AirbrakeSymbolicate
           error.backtrace.line.map {|l| Symbolicator.symbolicate_line(dsym, l)}
         end
       end
-      
+
       def symbolicate_line(dsym_file, line)
         binname = File.basename(dsym_file)
         if line[/#{binname}/] && loc = line[/0x\w+/]
@@ -68,7 +62,7 @@ module AirbrakeSymbolicate
       end
     end
   end
-  
+
   class Airbrake < ActiveResource::Base
     cattr_accessor :auth_token
 
@@ -77,7 +71,7 @@ module AirbrakeSymbolicate
         self.site = "https://#{a}.airbrake.io/" if a
         self.format = ActiveResource::Formats::XmlFormat
       end
-      
+
       def find(*arguments)
         arguments = append_auth_token_to_params(*arguments)
         super(*arguments)
@@ -85,7 +79,7 @@ module AirbrakeSymbolicate
 
       def append_auth_token_to_params(*arguments)
         raise RuntimeError.new("Airbrake.auth_token must be set!") if !auth_token
-        
+
         opts = arguments.last.is_a?(Hash) ? arguments.pop : {}
         opts = opts.has_key?(:params) ? opts : opts.merge(:params => {})
         opts[:params] = opts[:params].merge(:auth_token => auth_token)
@@ -93,10 +87,19 @@ module AirbrakeSymbolicate
         arguments
       end
     end
-    
+
   end
-  
-  class Error < Airbrake  
+
+  class Error < Airbrake
+
+    def app_version
+      if environment.respond_to?(:app_version)
+        environment.app_version
+      elsif environment.respond_to?(:application_version)
+        environment.application_version
+      end
+    end
+
   end
-  
+
 end
